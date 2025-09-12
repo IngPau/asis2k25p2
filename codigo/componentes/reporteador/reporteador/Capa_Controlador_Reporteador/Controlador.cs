dbbf8ee594a27f25d857a0f8a383f54392174c3c
Sergio Emmanuel Izeppi Valle <sergioizeppi@gmail.com>
2025-09-12 07:59:08 -0600

    Implementacion de Controlador de Ver Reportes

--- codigo/componentes/reporteador/reporteador/Capa_Controlador_Reporteador/Controlador.cs
+++ codigo/componentes/reporteador/reporteador/Capa_Controlador_Reporteador/Controlador.cs
@@ -8,5 +8,19 @@ namespace Capa_Controlador_Reporteador
 {
     class Controlador
     {
+        //inicio de codigo Sergio Izeppi, Carne: 0901-22-8946 en la fecha 11/09/2025
+
+        //public void EliminarReporte()
+        //{
+        //   sentencias.EliminarReporte();
+        //}
+
+        public DataTable ObtenerReportes()
+        {
+            return sentencias.ObtenerReporte();
+        }
+
+        //fin de codigo de Sergio Izeppi
     }
 }
+
